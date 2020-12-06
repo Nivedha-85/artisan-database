91f5fe43322adfe6143c23521a3fd08e1cf565d6
Nivedha-85 <60845904+Nivedha-85@users.noreply.github.com>
2020-12-06 10:19:20 +0530

    Add files via upload

--- raw_materials.js
+++ raw_materials.js
@@ -49,7 +49,16 @@ var con = mysql.createConnection({
 
 /*con.connect(function(err) {
     if (err) throw err;
-    var sql = "DELETE FROM RAW_MATERIALS WHERE material_id = 2";
+    var sql = "DELETE FROM RAW_MATERIALS WHERE material_name = 'Strings' ";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+  });*/
+
+  /*con.connect(function(err) {
+    if (err) throw err;
+    var sql = "DELETE FROM RAW_MATERIALS WHERE manufacturer = 'yzab' ";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("Number of records deleted: " + result.affectedRows);
